21227c6229a29b585c050d440be8377a2ddd89d7
PallaviPujari03 <pallavipujari03@gmail.com>
2023-05-05 03:58:21 +0530

    nodejs
    write a data

---
+++ hello.js
@@ -0,0 +1,26 @@
+
+
+
+
+console.log("Pallavi Pujari");
+var myfullname=("Pallavi Mallikarjun Pujari");//Variable Decleration
+console.log("Pallavi Mallikarjun Pujari");//Variable Initilization
+var mylove=("I Love only JS");
+console.log("I Love only JS");
+var firstname=("Pallavi");
+console.log("Pujari");
+var age;
+age="23";
+console.log(age);
+var address;
+address="At/Post-Sankh, Tal-Jath, Dist-Sangli";
+console.log(address);
+var pincode;
+pincode="416412";
+console.log(pincode);
+var gender;
+gender="Female";
+console.log(gender);
+var isMarried;
+isMarried="No";
+console.log(isMarried);
