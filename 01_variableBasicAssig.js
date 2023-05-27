0d4d2c458ca0599917c0edf6be4887c54ff12d9c
PallaviPujari03 <pallavipujari03@gmail.com>
2023-05-27 23:35:13 +0530

    assig
    assig

---
+++ 01_variableBasicAssig.js
@@ -0,0 +1,50 @@
+//1 Primitive Data Types:
+
+//1] Number
+console.log("---------Number--------")
+var rollNumber = 51;
+var price = 500;
+
+console.log("rollNumber: 51,","type:",typeof rollNumber);
+console.log("price:500,","type:",price);
+
+//2] Booleanue  --> true or false
+
+var isFit = true;
+var ishappy = true;
+
+console.log("isfit: true,","type:",typeof isFit);
+console.log("ishappy: true,","type:",typeof ishappy);
+
+//3] String --> Sequence of character enclosed in '' or ""
+
+var favouritMove = "Mithava";
+var favIplteam = "MI";
+var hobby = "Photography";
+
+console.log("favouritMove: Mithava,","type:",typeof favouritMove);
+console.log("favIplteam : MI,","type:",typeof favIplteam);
+console.log("hobby: photography,","type:",typeof hobby);
+
+console.log("");
+
+
+console.log("---------Difference between undfined and null data types-------");
+console.log("");
+confirm.log("undfined :- Variable is declared but it's not assigned to it.");
+console.log("null :- Variable is declared but but it's value assigned as a null value.");
+console.log("so the type of undefined as undefined and type of null as object");
+console.log("for null returns object this is an error in the language,it's not actually an object.");
+
+console.log("");
+console.log("---------Example of undefined and null--------");
+console.log("");
+
+//1 undefined
+console.log("Value of variable -->playerName:", playerName);
+console.log("Type of variable playerName is:", typeof playerName);
+
+//2 null
+var buildingName = null;
+console.log("Value of variable -->bulidingName:",buildingName);
+console.log("Type of variable bulidingName is:", typeof buildingName);
